0acd4a5a00cc4a07acff075b88b6deff546c394a
itsameWolf <44059936+itsameWolf@users.noreply.github.com>
2018-12-17 13:27:35 +0000

    Update Palpatronix.ino
    added encoder ISR

--- Palpatronix.ino
+++ Palpatronix.ino
@@ -15,13 +15,22 @@
 //Define pi for the hall effect sensor
 #define HallIn
 
-
+volatile long Ticks = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(EncoderA,INPUT);
   pinMode(EncoderB,INPUT);
 
+  attachInterrupt(digitalPinToInterrupt(EncoderA),Arising,RISING);
+  attachInterrupt(digitalPinToInterrupt(EncoderA),Afalling,FALLING);
+
+  attachInterrupt(digitalPinToInterrupt(EncoderB),Brising,RISING);
+  attachInterrupt(digitalPinToInterrupt(EncoderB),Bfalling,FALLING);
+  
+  //attachInterrupt(digitalPinToInterrupt(EncoderA),ChannelA,CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(EncoderB),ChannelB,CHANGE);
+  
   pinMode(EnableA,OUTPUT);
   pinMode(EnableB,OUTPUT);
   pinMode(CurrentCOntrolA,OUTPUT);
@@ -39,3 +48,93 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+////////////////////////////////////Encoder ISR////////////////////////////////////
+void Arising()
+{
+  if (digitalRead(ChannelB)
+  {
+    Ticks++;
+  }else{
+    Ticks--;
+  }
+}
+
+void Afalling()
+{
+  if (digitalRead(ChannelB))
+  {
+    Ticks--;
+  }else{
+    ticks++;
+  }
+}
+
+void Brising()
+{
+  if (digitalRead(ChannelA))
+  {
+    Ticks--;
+  }else{
+    Ticks++;
+  }
+}
+
+void Bfalling()
+{
+  if (digitalRead(ChannelA)
+  {
+    Ticks++;
+  }else{
+    Ticks--;
+  }
+}
+
+/*
+void ChannelA()
+{
+  if (digitalRead(EncoderA)
+  {
+    if (digitalRead(EncoderB)
+    {
+      Ticks--  
+    } else 
+    {
+      Ticks++
+    }
+  }else
+  {
+    if (digitalRead(EncoderB)
+    {
+      Ticks++  
+    } else 
+    {
+      Ticks--
+    }
+  }
+}
+
+void ChannelB()
+{
+  if (digitalRead(EncoderB)
+  {
+    if (digitalRead(EncoderA)
+    {
+      Ticks--  
+    } else 
+    {
+      Ticks++
+    }
+  }else
+  {
+    if (digitalRead(EncoderA)
+    {
+      Ticks++  
+    } else 
+    {
+      Ticks--
+    }
+  }
+}
+*/
+
