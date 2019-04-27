ba2f2ee28aa42aa5adfd4ef5fea83dd51ca83114
itsameWolf <44059936+itsameWolf@users.noreply.github.com>
2019-04-28 00:57:45 +0100

    stuff

--- compression soft sensor/Compression_analysis.py
+++ compression soft sensor/Compression_analysis.py
@@ -1,8 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-compressionFileName = r"C:\Users\Lupo\Documents\GitHub\Palpatronix\compression soft sensor\Ecoflex 00-20 Compression Test new_without screws.is_comp_RawData\Specimen_RawData_2.csv"
-hallEffectFileName = r"C:\Users\Lupo\Documents\GitHub\Palpatronix\compression soft sensor\compression 9 - 20 - raw.txt"
+compressionFileName = r"E:\Documents\GitHub\Palpatronix\compression soft sensor\Ecoflex 00-30 Compression Test new.is_comp_RawData\Specimen_RawData_1.csv"
+hallEffectFileName = r"E:\Documents\GitHub\Palpatronix\compression soft sensor\compression 6 - 30.txt"
 
 compressionData = np.loadtxt(compressionFileName, delimiter=",", skiprows=2)
 hallEffectData  = np.loadtxt(hallEffectFileName,delimiter=",")
@@ -13,12 +13,12 @@ title, compression = plt.subplots(3,1,sharex=True)
 
 title.suptitle("Compression Test with Hall Effect Sensor")
 
-compression[0].scatter(compressionData[:,0],compressionData[:,1])
-compression[1].scatter(compressionData[:,0],compressionData[:,2])
-compression[2].scatter(hallEffectData[:,1],hallEffectData[:,0])
+compression[1].scatter(compressionData[:,0],compressionData[:,1])
+compression[2].scatter(compressionData[:,0],compressionData[:,2])
+compression[0].scatter(hallEffectData[:,1],hallEffectData[:,0])
 
-compression[0].set_title("Displacement")
-compression[1].set_title("Load")
-compression[2].set_title("Hall Effect")
+compression[1].set_title("Displacement")
+compression[2].set_title("Load")
+compression[0].set_title("Hall Effect")
 
 plt.show()
